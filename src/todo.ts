3b8d491c1d1782a86f93bdc536990315c6a9780a
shu <syu220922@gmail.com>
2023-11-03 15:58:48 +0800

    feat(ts): 利用類別定義待辦事項

---
+++ src/todo.ts
@@ -0,0 +1,11 @@
+export class Todo {
+  constructor(id: number, content: string) {
+    this.id = id;
+    this.content = content;
+    this.hasFinished = false;
+  }
+  id: number;
+  content: string;
+  hasFinished: boolean;
+  finishDate: Date | undefined;
+}
