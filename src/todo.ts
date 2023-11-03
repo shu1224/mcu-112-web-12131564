018492903500af47566c76f618164995e53c2cae
shu <syu220922@gmail.com>
2023-11-03 16:38:13 +0800

    refactor(ts):變更待辦事項建構式

--- src/todo.ts
+++ src/todo.ts
@@ -1,11 +1,8 @@
 export class Todo {
-  constructor(id: number, content: string) {
-    this.id = id;
-    this.content = content;
-    this.hasFinished = false;
-  }
-  id: number;
-  content: string;
-  hasFinished: boolean;
-  finishDate: Date | undefined;
+  constructor(
+    public id: number,
+    public content: string,
+    public hasFinished = false
+  ) {}
+  finishDate?: Date;
 }
