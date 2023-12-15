5c79311ac11beb4f1f58c5728b937873edac4eee
shu <syu220922@gmail.com>
2023-12-15 15:28:28 +0800

    feat(task): 實作新增資料庫待辦事項資料

--- src/app/model/todo.ts
+++ src/app/model/todo.ts
@@ -1,9 +1,17 @@
 export class Todo {
-  constructor(
-    public id: number,
-    public content: string,
-    public hasFinished: boolean = false
-  ) {}
+  constructor(initData?: Partial<Todo>) {
+    Object.assign(this, initData);
+    if (this.hasFinished === undefined) {
+      this.hasFinished = false;
+    }
+  }
+
+  id!: number;
+
+  content!: string;
+
+  hasFinished!: boolean;
+
   finishDate?: Date;
 
   setFinished(finishDate: Date): void {
